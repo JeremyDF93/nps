1c2a5e70c6980577e7ceba4d12894ba53fea8bec
Jeremy Farmer <jeremydf93@hotmail.com>
2019-12-31 09:18:52 -0800

    add versioning

--- scripting/compile.sh
+++ scripting/compile.sh
@@ -1,31 +1,40 @@
-#!/bin/bash
+#!/bin/bash -e
 cd "$(dirname "$0")"
 
-SMSDK=/home/jeremy/Projects/alliedmodders/sourcemod
-NYXTOOLS=../nyxtools
+SMSDK=$HOME/Projects/alliedmodders/sourcemod
+NYXTOOLS_SDK=../nyxtools
+SPCOMP=spcomp
+
+###
+### You shouldn't need to edit below this line.
+###
+git_count() {
+  git rev-list --all --count
+}
+
+VERSION=`cat VERSION`
+MAJOR=`echo $VERSION | cut -d. -f1`
+MINOR=`echo $VERSION | cut -d. -f2`
+REVISION=`echo $VERSION | cut -d. -f3`
+BUILD_STRING="git$(git_count)"
+eval "echo \"$(cat include/nps_version.tpl)\"" > include/nps_version.inc
 
 test -e compiled || mkdir compiled
 
 if [[ $# -ne 0 ]]; then
-	for i in "$@";
-	do
-		smxfile="`echo $i | sed -e 's/\.sp$/\.smx/'`";
-		echo -e "Compiling $i...";
-		spcomp $i -iinclude -i$SMSDK/plugins/include -i$NYXTOOLS/scripting/include -ocompiled/$smxfile
-		RETVAL=$?
-		if [ $RETVAL -ne 0 ]; then
-			exit 1;
-		fi
-	done
+  for sourcefile in "$@"
+  do
+    smxfile="`echo $sourcefile | sed -e 's/\.sp$/\.smx/'`"
+    echo -e "\nCompiling $sourcefile..."
+    $SPCOMP $sourcefile -iinclude -i$SMSDK/plugins/include -i$NYXTOOLS_SDK/scripting/include \
+				-ocompiled/$smxfile -E
+  done
 else
-	for sourcefile in *.sp
-	do
-		smxfile="`echo $sourcefile | sed -e 's/\.sp$/\.smx/'`"
-		echo -e "Compiling $sourcefile ..."
-		spcomp $sourcefile -iinclude -i$SMSDK/plugins/include -i$NYXTOOLS/scripting/include -ocompiled/$smxfile
-		RETVAL=$?
-		if [ $RETVAL -ne 0 ]; then
-			exit 1;
-		fi
-	done
+  for sourcefile in *.sp
+  do
+    smxfile="`echo $sourcefile | sed -e 's/\.sp$/\.smx/'`"
+    echo -e "\nCompiling $sourcefile ..."
+    $SPCOMP $sourcefile -iinclude -i$SMSDK/plugins/include -i$NYXTOOLS_SDK/scripting/include \
+				-ocompiled/$smxfile -E
+  done
 fi
